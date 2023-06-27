ac67ad4cc3fe5f3d29338c1de8762101790f6228
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-06-27 14:01:08 -0400

    Fixed File structure and introduced rich's image files

--- kevin's files/main.js
+++ kevin's files/main.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d')
     canvas.width = 1000;
-    canvas.height = 500;
+    canvas.height = 700;
    
     class Game {
         constructor(width, height){
