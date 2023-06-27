ba15317474661f628ed38edd65b54cc2215dd82e
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-06-27 13:39:13 -0400

    successful merge

--- kevin's files/main.js
+++ kevin's files/main.js
@@ -1,10 +1,11 @@
 import {Player} from './player.js';
 import { InputHandler } from './input.js';
+import { Background } from './background.js';
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d')
-    canvas.width = 500;
+    canvas.width = 1000;
     canvas.height = 500;
    
     class Game {
@@ -13,13 +14,16 @@ window.addEventListener('load', function(){
             this.height = height;
             this.groundMargin = 50;
             this.speed = 3;
+            this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
         }
         update(deltaTime){
+            this.background.update();
             this.player.update(this.input.keys, deltaTime);
         }
         draw(context){
+            this.background.draw(context);
             this.player.draw(context);
         }
     }
