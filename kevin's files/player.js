a71a7e9ef1fe50063a1dd59039f3e9530a5835a5
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-06-27 11:47:16 -0400

    kevin's files

---
+++ kevin's files/player.js
@@ -0,0 +1,75 @@
+import { Running, Sitting, Jumping, Falling } from "./playerStates.js";
+
+export class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = 0;
+    this.y = this.game.height - this.height;
+    this.vy = 0;
+    this.weight = 1;
+    this.image = document.getElementById("player");
+    this.speed = 0;
+    this.maxSpeed = 10;
+    this.states = [
+      new Sitting(this),
+      new Running(this),
+      new Jumping(this),
+      new Falling(this),
+    ];
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame = 5;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+    this.currentState = this.states[0];
+    this.currentState.enter();
+  }
+  update(input, deltaTime) {
+    this.currentState.handleInput(input);
+    //horizontal movement
+    this.x += this.speed;
+    if (input.includes("ArrowRight"))
+      this.speed = this.maxSpeed; //right movement
+    else if (input.includes("ArrowLeft"))
+      this.speed = -this.maxSpeed; //left movement
+    else this.speed = 0; //stop moving
+    if (this.x < 0) this.x = 0; //left border
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width; //right
+    //vertical movement
+    this.y += this.vy;
+    if (!this.onGround()) this.vy += this.weight;
+    else this.vy = 0;
+    //sprite animation
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+        this.frameTimer += deltaTime;
+    }
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+  onGround() {
+    return this.y >= this.game.height - this.height;
+  }
+  setState(states) {
+    this.currentState = this.states[states];
+    this.currentState.enter();
+  }
+}
