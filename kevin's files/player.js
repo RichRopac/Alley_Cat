c9aae861346a5c34cbba749115a75b5d4992a0ee
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-06-27 12:42:57 -0400

    added ground padding

--- kevin's files/player.js
+++ kevin's files/player.js
@@ -6,7 +6,7 @@ export class Player {
     this.width = 100;
     this.height = 91.3;
     this.x = 0;
-    this.y = this.game.height - this.height;
+    this.y = this.game.height - this.height - this.game.groundMargin;
     this.vy = 0;
     this.weight = 1;
     this.image = document.getElementById("player");
@@ -66,7 +66,7 @@ export class Player {
     );
   }
   onGround() {
-    return this.y >= this.game.height - this.height;
+    return this.y >= this.game.height - this.height -this.game.groundMargin;
   }
   setState(states) {
     this.currentState = this.states[states];
